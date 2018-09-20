0769358cfb8eb72c8b0fb469b22da490a867060e
Jason Kao <jasonkao85@gmail.com>
2018-09-20 15:51:54 -0400

    data

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-const TILE_SIZE = 20;
+const TILE_SIZE = 16;
 const TILES_PER_ROW = 5;
 
 const BAR_PADDING = 20;
 const BAR_WIDTH = (TILES_PER_ROW * TILE_SIZE) + BAR_PADDING
 
 const colors = ["#ffd275", "#e8ae68", "#a57f60", "#e3a587", "#e48775"];
-const data = [42, 34, 12, 17, 53];
+let data = [42, 34, 12, 17, 53];
 
 const getTiles = num => {
   const tiles = [];
@@ -20,8 +20,8 @@ const getTiles = num => {
   return tiles;
 };
 
-function updateBar(num, i) {
-  const tiles = getTiles(num);
+function updateBar(d, i) {
+  const tiles = getTiles(d.age);
 
   const u = d3.select(this)
     .attr('transform', `translate(${i * BAR_WIDTH}, 300)`)
@@ -51,12 +51,32 @@ const updateBars = () => {
     .append('g')
     .merge(u)
     .style('fill', (d, i) => colors[i % colors.length])
-    .each(updateBar);
+    .each(updateBar);    
 
   u.exit().remove();    
 };
 
-updateBars();
+const initializeData = () =>
+  data = data
+    .filter(d => +d.year === 2016)
+    .map(d => ({
+      name: d.country,
+      year: +d.year,
+      age: +d.all
+    }))
+    .slice(0, 10);
+
+const initialize = () => {
+  initializeData();
+};
+
+d3.tsv("data/life-expectancy-WHO-2000-2016-simplified.tsv", (err, tsv) => {
+  data = tsv;
+  console.log(data[0])
+ 
+  initialize();
+  updateBars();
+});
 
 
 
