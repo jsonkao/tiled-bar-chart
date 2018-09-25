1c4171128aee2db375d9c2799b389b65a758d9b3
Jason Kao <jasonkao85@gmail.com>
2018-09-25 01:27:01 -0400

    yeet

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-const TILE_SIZE = 16;
+const TILE_SIZE = 15;
 const TILES_PER_ROW = 5;
-
 const BAR_PADDING = 20;
-const BAR_WIDTH = (TILES_PER_ROW * TILE_SIZE) + BAR_PADDING;
 const MAX_VALUE = 100;
 const NUM_VISIBLE_COUNTRIES = 10;
+
+const barWidth = (TILES_PER_ROW * TILE_SIZE) + BAR_PADDING;
 
 const colors = ["#ffd275", "#e8ae68", "#a57f60", "#e3a587", "#e48775"];
 let data = [42, 34, 12, 17, 53];
@@ -26,14 +26,14 @@ function updateBar(d, i) {
   const tiles = getTiles(d.age);
 
   const u = d3.select(this)
-    .attr('transform', `translate(${i * BAR_WIDTH}, 300)`)
+    .attr('transform', `translate(${i * barWidth}, 300)`)
     .selectAll('rect')
     .data(tiles);
 
   u.enter()
     .append('rect')
     .style('stroke', 'white')
-    .style('stroke-width', 0.5)
+    .style('stroke-width', '0.5')
     .style('shape-rendering', 'crispEdges')
     .merge(u)
     .attr('x', d => d.x)
@@ -76,11 +76,11 @@ const updateBars = () => {
 };
 
 const updateAxis = () => {
-  // const chartWidth = NUM_VISIBLE_COUNTRIES * BAR_WIDTH;
+  const chartWidth = NUM_VISIBLE_COUNTRIES * barWidth;
   const chartHeight = (MAX_VALUE / TILES_PER_ROW) * TILE_SIZE;
 
   const yScale = d3.scaleLinear().domain([0, MAX_VALUE]).range([chartHeight, 0]);
-  const yAxis = d3.axisRight().scale(yScale);
+  const yAxis = d3.axisRight().scale(yScale).tickSize(chartWidth);
 
   d3.select('.y.axis')
     .call(yAxis);
